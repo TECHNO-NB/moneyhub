fb45d449dc2e76910a00841b2d75c82e46a20822
NareshBhattarai4 <technologyandapplication@gmail.com>
2025-07-13 20:22:08 +0545

    add e is unused

--- src/app/add-coin/page.tsx
+++ src/app/add-coin/page.tsx
@@ -47,7 +47,7 @@ const Page: React.FC = () => {
           src="https://www.bing.com/th/id/OIP.ufLMWTZgKfXePoS2TERVjwHaHa?w=204&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
           height={200}
           width={200}
-          onInput={(e) => setFile}
+        
           className="rounded-lg"
         />
       </div>
