582d5c7958d7f7e36c2e377ebe12f9a1980d8ae6
NareshBhattarai4 <technologyandapplication@gmail.com>
2025-07-14 21:52:41 +0545

    fixed eslint error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-import { usePathname } from "next/navigation";
 import LayoutShell from "@/components/LayoutShell";
 
 const geistSans = Geist({
