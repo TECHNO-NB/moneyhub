1f7877a4408ee9acc92ffea9d7329fa798c4532e
NareshBhattarai4 <technologyandapplication@gmail.com>
2025-09-29 16:25:06 +0545

    fixed bug

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -3,22 +3,25 @@
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
-import topuprate from "../../public/topup-rate.jpg";
 
 import "swiper/css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function Slider() {
-  const [data,setData]=useState([])
+interface Banner {
+  id: string;
+  image: string;
+}
 
-    useEffect(() => {
+export default function Slider() {
+  const [data, setData] = useState<Banner[]>([]);
+
+  useEffect(() => {
     const fetchAllImages = async () => {
       try {
         const res = await axios.get(
           `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users/get-all-banner`
         );
-
         setData(res.data.data);
       } catch (error) {
         console.log(error);
@@ -26,6 +29,7 @@ export default function Slider() {
     };
     fetchAllImages();
   }, []);
+
   return (
     <div className="w-full max-w-5xl mx-auto mt-6">
       <Swiper
@@ -39,42 +43,31 @@ export default function Slider() {
         slidesPerView={1}
         className="rounded-2xl shadow-lg"
       >
-
-        {data && data.map((val:any,index)=>(
-
-          <SwiperSlide key={index}>
-          <div className="w-full h-[300px]">
-            <Image
-              src={val.image}
-              alt="Free Fire"
-              fill
-              className="object-fit rounded-2xl border-2 border-yellow-400"
+        {data.length > 0 ? (
+          data.map((val, index) => (
+            <SwiperSlide key={val.id || index}>
+              <div className="relative w-full h-[250px] md:h-[340px]">
+                <Image
+                  src={val.image}
+                  alt={`Banner ${index + 1}`}
+                  fill
+                  className="object-cover rounded-2xl border-2 border-yellow-400"
+                />
+              </div>
+            </SwiperSlide>
+          ))
+        ) : (
+          <SwiperSlide>
+            <div className="relative w-full h-[300px]">
+              <Image
+                src="https://tse4.mm.bing.net/th/id/OIP.JfNdlwKPrz_pQYu9ud1pNQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3"
+                alt="Default Banner"
+                fill
+                className="object-cover rounded-2xl border-2 border-yellow-400"
               />
-          </div>
-        </SwiperSlide>
-            ))}
-
-        {/* <SwiperSlide>
-          <div className="w-full h-[300px] border-2 border-yellow-400">
-            <Image
-              src={topuprate}
-              alt="Gaming"
-              fill
-              className="object-cover rounded-2xl border-2 border-yellow-400"
-            />
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="w-full h-[300px] border-2 border-yellow-400">
-            <Image
-              src="https://tse4.mm.bing.net/th/id/OIP.JfNdlwKPrz_pQYu9ud1pNQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3"
-              alt="Gamer Setup"
-              fill
-              className="object-cover rounded-2xl border-2 border-yellow-400"
-            />
-          </div>
-        </SwiperSlide> */}
+            </div>
+          </SwiperSlide>
+        )}
       </Swiper>
     </div>
   );
